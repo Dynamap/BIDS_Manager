23d421232393ad3ac4f346fdef56e4cfe868c139
chrisferreyra13@gmail.com <chrisferreyra13@gmail.com>
2023-05-04 10:15:48 +0200

    removed releases folder

--- releases/1.4.3/deface_needs/mango-script.bat
+++
@@ -1,2 +0,0 @@
-@ echo off
-"C:\Program Files\Mango\jre7/bin/java.exe" -Djava.awt.headless=true -Xms64M -Xmx512M -XX:MaxDirectMemorySize=1536M -cp "C:\\Program Files\\Mango\\Mango.jar" edu.uthscsa.ric.cli.script.MangoScript %*
